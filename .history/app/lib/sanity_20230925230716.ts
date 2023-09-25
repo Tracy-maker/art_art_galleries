97be8e7da6ad061a3c0cb7cd97cc114e6a91477d
Tracy-maker <ydlvns@gmail.com>
2023-09-25 23:08:43 +1000

    add the data in the sanity

--- .history/app/lib/sanity_20230925230716.ts
+++ .history/app/lib/sanity_20230925230716.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = 'w3gmnen9';
 const dataset = "production";
-const apiVersion = "2023-12-31";
+const apiVersion = "2023-11-31";
 
 export const client = createClient({
   projectId,
