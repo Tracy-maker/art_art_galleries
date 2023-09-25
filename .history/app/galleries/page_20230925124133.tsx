f8fb4b27644bc43d30ad84a87bed0a3835765c54
Tracy-maker <ydlvns@gmail.com>
2023-09-25 12:59:31 +1000

    add the gallery page

---
+++ .history/app/galleries/page_20230925124133.tsx
@@ -0,0 +1,12 @@
+export default function galleries() {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="font-custom leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-lg sm:leading-10 md:text-xl md:leading-13">
+          Greetings to my art sanctuary! Should any piece stir your soul, a
+          simple email will bridge our worlds. Let our artistic dialogue begin.
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/app/galleries/page_20230925125750.tsx
@@ -0,0 +1,18 @@
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+
+export default function galleries() {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <div className="flex font-custom leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-lg sm:leading-10 md:text-xl md:leading-13">
+          Greetings to my art sanctuary! Should any piece stir your soul, a
+          simple email will bridge our worlds. Let our artistic dialogue begin.
+          <Link href="mailto:ydlvns@gmail.com" target="_email">
+            <EnvelopeIcon className="w-16 h-16 text-blue-500" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
