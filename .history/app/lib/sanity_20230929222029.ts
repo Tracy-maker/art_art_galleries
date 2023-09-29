f13542a700d45cbfbcb56829d983bd89fbb26b1e
Tracy-maker <ydlvns@gmail.com>
2023-09-29 22:21:10 +1000

    add the new content

--- .history/app/lib/sanity_20230929222029.ts
+++ .history/app/lib/sanity_20230929222029.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = 'w3gmnen9';
 const dataset = "production";
-const apiVersion = "2023-10-01";
+const apiVersion = "2023-10-1";
 
 export const client = createClient({
   projectId,
