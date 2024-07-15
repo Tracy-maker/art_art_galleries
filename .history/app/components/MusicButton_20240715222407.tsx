6276bb0459a2b260bd89453cc989e8073abef871
Tracy-maker <ydlvns@gmail.com>
2024-07-15 22:26:46 +1000

    fix layout

--- .history/app/components/MusicButton_20240715222407.tsx
+++ .history/app/components/MusicButton_20240715222407.tsx
@@ -40,6 +40,7 @@ export default function MusicButton() {
 
   return (
     <>
+      
       <Menu as="div" className="relative inline-block text-left">
         <div>
           <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 sm:text-base">
@@ -99,6 +100,7 @@ export default function MusicButton() {
           <MusicalNoteIcon className="w-6 h-6" />
         )}
       </button>
+
     </>
   );
 }
